baf6a2cc2b671cc57a8ca65838fd2a4d92c63563
Gareth Shaw <Gareth Shaw>
2016-07-22 11:55:05 +1200

    Feet move the body.

--- ShoppingCat/Assets/Scripts/FeetController.cs
+++ ShoppingCat/Assets/Scripts/FeetController.cs
@@ -3,40 +3,47 @@ using System.Collections;
 
 public class FeetController : MonoBehaviour {
 
-    private GameObject leftFoot;
-    private GameObject rightFoot;
+    public GameObject upperBody;
+    public GameObject leftFoot;
+    public GameObject rightFoot;
 
     private const float MAX_SPEED = 5.0f;
     private Vector3 MOVE_FORWARD = new Vector3(0, 0, MAX_SPEED);
 
-
-    // TODO: Use
     private const float MAX_STRIDE = 10.0f;
 
     private Vector3 VERTICAL_SPEED = new Vector3(0, 2.0f, 0);
     private const float MAX_HEIGHT = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-        rightFoot = transform.Find("Right").gameObject;
-        leftFoot = transform.Find("Left").gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // TODO: Options: only allow one foot moving at a time.
         updateFoot(rightFoot, "Fire1");
-        updateFoot(leftFoot, "Fire2");   
+        updateFoot(leftFoot, "Fire2");
+
+        Vector3 upperBodyTarget = upperBody.transform.position;
+        upperBodyTarget.z = (rightFoot.transform.position.z + leftFoot.transform.position.z) / 2;
+        upperBody.transform.position = Vector3.MoveTowards(upperBody.transform.position, upperBodyTarget, MAX_SPEED * Time.deltaTime);
+
+        if (Vector3.Distance(leftFoot.transform.position, rightFoot.transform.position) >= MAX_STRIDE)
+        {
+            // TODO: Trigger event
+        }
     }
 
     private void updateFoot(GameObject foot, string input)
     {
         Vector3 movement = Vector3.zero;
         bool inputPressed = Input.GetButton(input);
-        if (inputPressed)
+        movement += getVerticalSpeed(foot.transform.position.y, inputPressed);
+        if (movement.y != 0 || inputPressed)
         {
             movement += MOVE_FORWARD;
         }
-        movement += getVerticalSpeed(foot.transform.position.y, inputPressed);
 
         foot.transform.Translate(movement * Time.deltaTime);
     }
