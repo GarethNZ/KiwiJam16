ba949553d6cddb00e984ee3f4dc46da08630b738
Gareth Shaw <Gareth Shaw>
2016-07-22 08:30:04 +1200

    More Unity things

---
+++ ShoppingCat/Assets/Scripts/FeetController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeetController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
