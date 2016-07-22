6767665d7e07fe0c06c79f0609c41a2d49b80c25
Gareth Shaw <Gareth Shaw>
2016-07-23 00:38:22 +1200

    Pickup spawner and start on spine twisting

---
+++ ShoppingCat/Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour {
+
+    public GameObject[] pickups;
+    public GameObject[] targets;
+
+    private float PERIOD = 0.5f;
+
+    private float lastTime = 0;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.time > (lastTime + PERIOD))
+        {
+            lastTime += Time.time;
+            int pickup = Random.Range(0, pickups.Length);
+            int position = Random.Range(0, targets.Length);
+            Vector3 spawnPoint = targets[position].transform.position;
+            spawnPoint.y = Random.Range(0, 5.0f);
+            GameObject newPickup = Instantiate(pickups[pickup], spawnPoint, targets[position].transform.rotation) as GameObject;
+            newPickup.transform.SetParent(this.transform);
+        }
+	}
+}
---
+++ ShoppingCat/Assets/Scripts/SpineTwistController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpineTwistController : MonoBehaviour {
+
+    public string input;
+    public Transform jointToTwist;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float axisValue = Input.GetAxis(input);
+        Vector3 rotation = Vector3.zero;
+        rotation.x += axisValue;
+        jointToTwist.Rotate(rotation);
+    }
+}
