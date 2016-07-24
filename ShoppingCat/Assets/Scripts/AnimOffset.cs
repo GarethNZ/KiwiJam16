f95839417e9ee7d68719fc70cf16bed1f063da32
Chris Lyne <chris@funnylyne.com>
2016-07-24 17:20:35 +1200

    Intro + images

--- ShoppingCat/Assets/Scripts/AnimOffset.cs
+++ ShoppingCat/Assets/Scripts/AnimOffset.cs
@@ -6,7 +6,7 @@ public class AnimOffset : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		//GetComponent<Animator>()["Plane"].time = Random.Range(0.0f, GetComponent<Animation>()["Plane"].length);
+
 		GetComponent<Animator>().ForceStateNormalizedTime(UnityEngine.Random.Range(0.0f, 1.0f));
 	
 	}
---
+++ ShoppingCat/Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour {
+
+	bool ready = false;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonUp ("Fire1")) {
+			GetComponent<Animator> ().SetTrigger ("startGame");
+			ready = true;
+		}
+
+		if (Input.GetButton ("Fire1") && ready == true) {
+			Application.LoadLevel("main");
+
+		}
+	}
+
+}
+
