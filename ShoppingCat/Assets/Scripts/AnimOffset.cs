04242b42be866adbc4d72283f2355fa4e98e2aa8
Chris Lyne <chris@funnylyne.com>
2016-07-24 15:17:57 +1200

    GUI layout

---
+++ ShoppingCat/Assets/Scripts/AnimOffset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimOffset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//GetComponent<Animator>()["Plane"].time = Random.Range(0.0f, GetComponent<Animation>()["Plane"].length);
+		GetComponent<Animator>().ForceStateNormalizedTime(UnityEngine.Random.Range(0.0f, 1.0f));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
